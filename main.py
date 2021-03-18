d6c7862d87c43dedd55f7d4d4c859c2d77fd8bc4
Salvador Guzman <guzmansalv@gmail.com>
2021-03-18 08:46:41 -0700

    working so far

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import atexit
 import json
 import logging
 import os
+import psycopg2
 import typing
 from typing import Dict
 from typing import List
@@ -13,15 +14,34 @@ import lib.duolingo as duolingo
 
 
 env_json_file: str = os.path.abspath('./env.json')
+sess_file: str = os.path.abspath('./duo.sess')
 env = {}
-sessfile: str = os.path.abspath('./.duo.sess')
 lingo = None
+conn = None
+
+
+def init_sql() -> None:
+    db: str = env['PG_DB']
+    usr: str = env['PG_USER']
+    host: str = env['PG_HOST']
+    port: str = env['PG_PORT']
+    pss: str = env['PG_PASS']
+
+    global conn
+    conn = psycopg2.connect(database=db,
+                            user=usr,
+                            password=pss,
+                            host=host,
+                            port=port)
+
+    logging.info('Successfully connected to db')
 
 
 def init_env() -> None:
     for e in env['env']:
         if e in env:
-            logging.info('Found env var "%s" in file with default value "%s"', e, env[e])
+            msg: str = 'Found env var "%s" in file with default value "%s"'
+            logging.info(msg, e, env[e])
         else:
             env[e] = os.environ[e]
             logging.info('Found env var "%s" with value "%s"', e, env[e])
@@ -38,7 +58,9 @@ def init_duo_log() -> None:
     usr: str = env['DUO_USER']
     pss: str = env['DUO_PASS']
 
-    lingo = duolingo.Duolingo(username=usr, password=pss)
+    lingo = duolingo.Duolingo(username=usr,
+                              password=pss,
+                              session_file=sess_file)
     logging.info('Success logged in with user "%s" and pass "%s"', usr, pss)
     logging.info(lingo)
 
@@ -67,6 +89,7 @@ def init() -> None:
     init_env()
     init_atexit()
     init_duo_log()
+    init_sql()
 
 
 def main() -> None:
